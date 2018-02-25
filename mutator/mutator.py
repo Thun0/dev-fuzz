686a020abec914c3ae27a067c1c8c7703973270c
Thun <pjankow6@gmail.com>
2018-02-25 11:36:08 +0100

    Added mutator functions + test

--- mutator/mutator.py
+++ mutator/mutator.py
@@ -8,25 +8,42 @@ class Mutator:
     Set of mutator functions
     """
 
-    @staticmethod
-    def randomize_bytes(data):
+    def __init__(self, data):
+        self.data = data.copy()
+
+    def randomize_bytes(self):
         """
         Randomizes bytes in data. Maximum bytes to change is specified in settings.
-        :param data: data to mutate
         """
-        max_bytes_to_change = int(len(data)*config["mutator"]["max_mutated_bytes"])
+        max_bytes_to_change = int(len(self.data)*config["mutator"]["max_mutated_bytes"])
         if max_bytes_to_change == 0:
             max_bytes_to_change = 1
         bytes_to_change = random.randint(1, max_bytes_to_change)
-        Mutator.randomize_n_bytes(data, bytes_to_change)
+        Mutator.randomize_n_bytes(self.data, bytes_to_change)
 
-    @staticmethod
-    def randomize_n_bytes(data, n):
+    def randomize_n_bytes(self, n):
         """
         Randomizes n bytes in data.
-        :param data: data to mutate
         :param n: number of bytes to randomize
         """
         for _ in range(n):
-            idx = random.randint(0, len(data) - 1)
-            data[idx] = random.randint(0, 255)
+            idx = random.randint(0, len(self.data) - 1)
+            self.data[idx] = random.randint(0, 255)
+
+    def remove_n_bytes(self, n):
+        del self.data[-n:]
+
+    def remove_n_bytes_at(self, idx, n):
+        del self.data[idx:idx+n]
+
+    def append_n_random_bytes(self, n):
+        for _ in range(n):
+            self.data.append(random.randint(0, 255))
+
+    def append_bytes(self, bytes_list):
+        self.data.extend(bytes_list)
+
+    def insert_bytes(self, idx, bytes_list):
+        bytes_list.reverse()
+        for b in bytes_list:
+            self.data.insert(idx, b)
---
+++ mutator/test.py
@@ -0,0 +1,28 @@
+from mutator import Mutator
+
+sample_data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+m1 = Mutator(sample_data)
+m1.randomize_n_bytes(5)
+print(m1.data)
+
+m1 = Mutator(sample_data)
+m1. remove_n_bytes(8)
+print(m1.data)
+
+m1 = Mutator(sample_data)
+m1.remove_n_bytes_at(7, 3)
+print(m1.data)
+
+m1 = Mutator(sample_data)
+m1.append_n_random_bytes(5)
+print(m1.data)
+
+bytes_list = ['a', 'b', 'c']
+m1 = Mutator(sample_data)
+m1.append_bytes(bytes_list)
+print(m1.data)
+
+m1 = Mutator(sample_data)
+m1.insert_bytes(4, bytes_list)
+print(m1.data)
